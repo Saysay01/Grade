cbcc9ecfca89d362cbf7d4ce441e35ee8751a413
Saysay01 <152484180+Saysay01@users.noreply.github.com>
2024-03-29 14:33:36 +0700

    Create Command3.java

---
+++ Command3.java
@@ -0,0 +1,40 @@
+package grade;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Command2 {
+    public static void main(String[] args) {
+        
+        try {
+            String name, grade, input;
+            int score;
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a grade: ");
+            input = sc.nextLine();
+            sc.close();
+
+            FileReader infile = new FileReader("D:/java/Grade.txt");
+            sc = new Scanner(infile); // Aggregation or Composite
+            
+            while (sc.hasNext()) {
+                name = sc.next();
+                grade = sc.next();
+                score = sc.nextInt();
+                if (grade.equals(input)) {
+                    System.out.println(name + "\t" + score);
+                }else{
+                    System.out.println(name + "\t" + grade + "\t" + score);
+                }
+            }
+            sc.close();
+
+        } catch (IOException e) {
+            System.err.println("An error occurred.");
+        }
+        
+    }
+
+}
